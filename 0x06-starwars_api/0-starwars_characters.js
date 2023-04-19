1d2c131838ddf7ad6c3507045e0f5cdeb8eb3ac7
Tishly <ekaetteessien27@gmail.com>
2023-04-19 21:32:42 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,35 @@
 #!/usr/bin/node
-
+const argv = process.argv;
+const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
+const urlMovie = `${urlFilm}${argv[2]}/`;
 
 const request = require('request');
 
-const movieId = process.argv[2];
+request(urlMovie, function (error, response, body) {
+  if (error == null) {
+    const fbody = JSON.parse(body);
+    const characters = fbody.characters;
 
-if (!movieId) {
-  console.error('Please provide a movie ID as the first argument');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}`;
-
-request(apiUrl, { json: true }, (err, res, body) => {
-  if (err) {
-    console.error('Error occurred while fetching movie data', err);
-    process.exit(1);
+    if (characters && characters.length > 0) {
+      const limit = characters.length;
+      CharRequest(0, characters[0], characters, limit);
+    }
+  } else {
+    console.log(error);
   }
-
-  if (res.statusCode !== 200) {
-    console.error(`Unexpected response status code: ${res.statusCode}`);
-    process.exit(1);
-  }
-
-  body.characters.forEach((charUrl) => {
-    request(charUrl, { json: true }, (err, res, charData) => {
-      if (err) {
-        console.error(`Error occurred while fetching character data for ${charUrl}`, err);
-        return;
-      }
-
-      if (res.statusCode !== 200) {
-        console.error(`Unexpected response status code while fetching character data for ${charUrl}: ${res.statusCode}`);
-        return;
-      }
-
-      console.log(charData.name);
-    });
-  });
 });
+
+function CharRequest (idx, url, characters, limit) {
+  if (idx === limit) {
+    return;
+  }
+  request(url, function (error, response, body) {
+    if (!error) {
+      const rbody = JSON.parse(body);
+      console.log(rbody.name);
+      idx++;
+      CharRequest(idx, characters[idx], characters, limit);
+    } else {
+      console.error('error:', error);
+    }
+  });
